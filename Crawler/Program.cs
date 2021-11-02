ea8bed8ef31b9ecbafa899805a6bdb4baa5d89f8
Bpmiller32 <89849055+Bpmiller32@users.noreply.github.com>
2021-11-02 00:47:45 -0700

    Updated Crawler service to working proof of concept for SmartMatch files. Downloads files from epf.usps.com using PuppeteerSharp.

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -5,9 +5,23 @@ using System.Collections.Generic;
 using System;
 using System.Threading.Tasks;
 using PuppeteerSharp;
+using PuppeteerSharp.Input;
+using HtmlAgilityPack;
+using System.Linq;
 
 namespace Crawler
 {
+    class UspsFile
+    {
+        public string Name { get; set; }
+        public bool Downloaded { get; set; }
+        public string Date { get; set; }
+        public string Size { get; set; }
+
+        public string ProductKey { get; set; }
+        public string FileId { get; set; }
+    }
+
     class Program
     {
         static async Task Main(string[] args)
@@ -21,13 +35,13 @@ namespace Crawler
             LaunchOptions options = new LaunchOptions()
             {
                 Headless = false
-
             };
 
             using (Browser browser = await Puppeteer.LaunchAsync(options))
             {
                 using (Page page = await browser.NewPageAsync())
                 {
+                    // Set up headers for requests
                     // Dictionary<string, string> headers = new Dictionary<string, string>()
                     // {
                     //     {@"user-agent", @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"},
@@ -36,13 +50,18 @@ namespace Crawler
                     // };
                     // await page.SetExtraHttpHeadersAsync(headers);
 
+                    // Set up browser behavior and options
+                    ClickOptions doubleClick = new ClickOptions()
+                    {
+                        Button = MouseButton.Left,
+                        ClickCount = 2
+                    };
+
+                    await page.Client.SendAsync(@"Page.setDownloadBehavior", new { behavior = @"allow", downloadPath = @"C:\Users\billy\Desktop" });
+
                     try
                     {
-                        // page.Response += (sender, e) =>
-                        // {
-                        //     System.Console.WriteLine(e.Response);
-                        // };
-
+                        // Navigate to download portal page
                         await page.GoToAsync("https://epf.usps.gov/");
 
                         await page.WaitForSelectorAsync(@"#email");
@@ -53,26 +72,76 @@ namespace Crawler
 
                         await page.ClickAsync(@"#login");
 
+                        await page.WaitForSelectorAsync(@"#r1");
+                        await page.ClickAsync(@"#r1");
+                        await Task.Delay(5000);
                         await page.WaitForSelectorAsync(@"#r2");
                         await page.ClickAsync(@"#r2");
+                        await Task.Delay(5000);
 
-                        Thread.Sleep(5000);
-                        
-                        string html = page.GetContentAsync().Result;
-                        // XmlDocument xml = new XmlDocument();
-                        // xml.LoadXml(html);
-                        // XmlNodeList nodes = xml.SelectNodes("//");
+                        await page.WaitForSelectorAsync(@"#tblFileList > tbody");
+
+                        // Arrrived at download portal page, pull page HTML
+                        HtmlDocument doc = new HtmlDocument();
+                        doc.LoadHtml(page.GetContentAsync().Result);
+
+                        HtmlNodeCollection fileRows = doc.DocumentNode.SelectNodes(@"/html/body/div[2]/table/tbody/tr/td/div[3]/table/tbody/tr/td/div/table/tbody/tr");
+
+                        // Format downloadables into list
+                        List<UspsFile> fileList = new List<UspsFile>();
+                        foreach (var fileRow in fileRows)
+                        {
+                            UspsFile file = new UspsFile();
+                            file.Name = fileRow.ChildNodes[5].InnerText.Trim();
+                            file.Date = fileRow.ChildNodes[4].InnerText.Trim();
+                            file.Size = fileRow.ChildNodes[6].InnerText.Trim();
+
+                            file.ProductKey = fileRow.Attributes[0].Value.Trim().Substring(19, 5);
+                            file.FileId = fileRow.Attributes[1].Value.Trim().Substring(3, 7);
+                            if (fileRow.ChildNodes[1].InnerText.Trim() == "Downloaded")
+                            {
+                                file.Downloaded = true;
+                            }
+
+                            fileList.Add(file);
+                        }
+
+                        // Download files
+                        foreach (var file in fileList)
+                        {
+                            bool isTar = file.Name.Contains(".tar");
+
+                            if ((file.Downloaded == true) && (isTar == true))
+                            {
+                                await page.EvaluateExpressionAsync(@"getFileForDownload(" + file.ProductKey + "," + file.FileId + ",rw_" + file.FileId + ");");
+                                await Task.Delay(5000);
+                            }
+                        }
+
+                        // Poll and check that all files are finished downloading
+                        // TODO make this recursive
+                        string[] files = Directory.GetFiles(@"C:\Users\billy\Desktop", @"*.crdownload");
+                        foreach (var file in files)
+                        {
+                            if (File.Exists(file))
+                            {
+                                // Wait for 60s
+                                System.Console.WriteLine("waiting for... :\t" + file);
+                                await Task.Delay(60000);
+                            }
+                        }
 
 
+                        // Move all completed files to an output folder
+                        string[] subDirFiles = Directory.GetFiles(@"C:\Users\billy\Desktop", @"*.tar");
 
-                        // CookieParam[] cookies = page.GetCookiesAsync().Result;
+                        foreach (var file in files)
+                        {
+                            File.Move(file, @"C:\Users\billy\Desktop\Done\" + Path.GetFileName(file), true);
+                        }
 
+                        System.Console.WriteLine(@"All done!");
 
-
-
-                        File.WriteAllText(@"C:\Users\billy\Desktop\usps.html", html);
-
-                        System.Console.WriteLine("stop");
                     }
                     catch (System.Exception e)
                     {
