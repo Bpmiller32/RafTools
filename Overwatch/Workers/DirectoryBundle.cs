27fecd789abee15f03d02b8a9e654e22d268e5e8
Bpmiller32 <bpmiller32@gmail.com>
2022-01-13 22:36:52 -0800

    Testing new Github method, housekeeping

--- Overwatch/Workers/DirectoryBundle.cs
+++
@@ -1,9 +0,0 @@
-namespace OverwatchApi.Workers
-{
-    public class DirectoryStatus
-    {
-        public bool SmartMatch { get; set; }
-        public bool Parascript { get; set; }
-        public bool RoyalMail { get; set; }
-    }
-}
