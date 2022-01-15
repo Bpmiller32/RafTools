f8c3bcc5f158ed6c2b4819f6052fe3271f3a5688
Bpmiller32 <89849055+Bpmiller32@users.noreply.github.com>
2022-01-14 20:13:17 -0800

    Implemented appsettings using IConfiguration method/POCO hybrid, replaced Task.Delay inputs with TimeSpans for clarity, fixed bug with Parascript download

---
+++ Crawler/Crawler.App/AppSettings.cs
@@ -0,0 +1,17 @@
+using System.IO;
+
+namespace Crawler.Data
+{
+    public class AppSettings
+    {
+        public bool ServiceEnabled { get; set; } = true;
+        
+        public string DownloadPath { get; set; } = Directory.GetCurrentDirectory() + @"\Downloads";
+        
+        public int ExecDay { get; set; } = 15;
+        public int ExecHour { get; set; } = 7;
+        public int ExecMinute { get; set; } = 59;
+        public int ExecSecond { get; set; } = 59;
+    }
+}
+
