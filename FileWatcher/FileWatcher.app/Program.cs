e6380ef693e2d38312c20d9fe3ccd23a5aedc21e
Bpmiller32 <89849055+Bpmiller32@users.noreply.github.com>
2022-07-05 20:16:50 -0700

    Simple FileWatcher app, sends a Discord message when a file changes

---
+++ FileWatcher/FileWatcher.app/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("Hello, World!");
+
+async Task SendDiscordMessage(string message)
+{
+    using HttpClient httpClient = new();
+    Dictionary<string, string> postValues = new()
+        {
+            { "content", message + " <@148237136288546816>" }
+        };
+
+    FormUrlEncodedContent httpContent = new(postValues);
+
+    await httpClient.PostAsync("https://discord.com/api/webhooks/799379913458843710/XytHRu3A8dX-1hXWvVvGKUBRjnf43rWbkcn4OoTacVAxzDaCEtYqRs4hxS91HVN53-J0", httpContent);
+}
+
+async Task<bool> CheckFile(string filePath)
+{
+    DateTime lastWriteTime = new FileInfo(filePath).LastWriteTime;
+    DateTime now = DateTime.Now;
+
+    if ((now - lastWriteTime).TotalMinutes <= 1)
+    {
+        System.Console.WriteLine("File has been changed");
+        await SendDiscordMessage("File has been changed");
+        return true;
+    }
+
+    return false;
+}
+
+while (true)
+{
+    bool isFinished = false;
+
+    if (File.Exists(@"C:\ProgramData\RAF\ArgosyPost\Save\00201889.dr"))
+    {
+        isFinished = await CheckFile(@"C:\ProgramData\RAF\ArgosyPost\Save\00201889.dr");
+    }
+    if (isFinished)
+    {
+        break;
+    }
+
+    if (File.Exists(@"C:\ProgramData\RAF\ArgosyPost\Save\00201889_A1.dr"))
+    {
+        isFinished = await CheckFile(@"C:\ProgramData\RAF\ArgosyPost\Save\00201889_A1.dr");
+    }
+    if (isFinished)
+    {
+        break;
+    }
+
+    if (File.Exists(@"C:\ProgramData\RAF\ArgosyPost\Save\00201889_A2.dr"))
+    {
+        isFinished = await CheckFile(@"C:\ProgramData\RAF\ArgosyPost\Save\00201889_A2.dr");
+    }
+    if (isFinished)
+    {
+        break;
+    }
+
+    await Task.Delay(TimeSpan.FromSeconds(60));
+}
