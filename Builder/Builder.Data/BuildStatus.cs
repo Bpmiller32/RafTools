16ff10f7ef41faf6b6aad299cd3150ee6fde8582
Bpmiller32 <89849055+Bpmiller32@users.noreply.github.com>
2022-07-13 18:48:25 -0700

    Cleanup

--- Builder/Builder.Data/BuildStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace Builder.App.Utils;
-
-public enum BuildStatus
-{
-    Ready,
-    InProgress,
-    Error,
-    Disabled
-}
--- Builder/Builder.Data/SocketResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Builder.App.Utils;
-
-public class SocketResponse
-{
-    public BuildStatus Status { get; set; }
-    public int Progress { get; set; }
-    public string CurrentBuild { get; set; }
-    public List<string> AvailableBuilds { get; set; }
-}
