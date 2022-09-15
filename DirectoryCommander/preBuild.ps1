9543f5e0d6307ec92e2c1efdc72767d6c3376807
Bpmiller32 <89849055+Bpmiller32@users.noreply.github.com>
2022-09-14 21:03:54 -0700

    Another reorg, folder naming change

--- DirectoryCommander/preBuild.ps1
+++ DirectoryCommander/preBuild.ps1
@@ -9,45 +9,27 @@ $BaseDirectory = Get-Location
 
 # Switch and build project based on argument
 if ($Project -eq "Crawler") {
-    Set-Location .\Crawler
+    Set-Location .\Crawler.App
     dotnet build
     Set-Location -Path $BaseDirectory
 }
 if ($Project -eq "Builder") {
-    Set-Location .\Builder
+    Set-Location .\Builder.App
     dotnet build
     Set-Location -Path $BaseDirectory
 }
 if ($Project -eq "Tester") {
-    Set-Location .\Tester
+    Set-Location .\Tester.App
     dotnet build
     Set-Location -Path $BaseDirectory
 }
 if ($Project -eq "Common") {
-    Set-Location .\Common
+    Set-Location .\Common.Data
     dotnet build
     Set-Location -Path $BaseDirectory
 }
 if ($Project -eq "UI") {
     Set-Location .\UI
-    npm install
-    Set-Location -Path $BaseDirectory
-}
-
-# Build all
-if ($Project -eq "Services") {
-    Set-Location -Path $BaseDirectory
-    dotnet build
-    Copy-Item -Path .\Builder\BuildUtils -Destination .\Builder\bin\Debug\net6.0-windows -Recurse -Force
-    Set-Location -Path $BaseDirectory    
-}
-if ($Project -eq "All") {
-    Set-Location -Path $BaseDirectory
-    dotnet build
-    Copy-Item -Path .\Builder\BuildUtils -Destination .\Builder\bin\Debug\net6.0-windows -Recurse -Force
-    Set-Location -Path $BaseDirectory
-    
-    Set-Location -Path .\Ui
     npm install
     Set-Location -Path $BaseDirectory
 }
